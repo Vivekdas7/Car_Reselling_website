6efb28885d66e6ee31988f25ec1c97045e116b61
VIVEK DAS <123769894+Vivekdas7@users.noreply.github.com>
2024-01-08 00:47:03 +0530

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,13 @@
+fn drink(beverage: &str) {
+   // You shouldn't drink too much sugary beverages.
+    if beverage == "lemonade" {
+        if cfg!(panic="abort"){ println!("This is not your party. Run!!!!");}
+        else{ println!("Spit it out!!!!");}
+    }
+    else{ println!("Some refreshing {} is all I need.", beverage); }
+}
+
+fn main() {
+    drink("water");
+    drink("lemonade");
+}
